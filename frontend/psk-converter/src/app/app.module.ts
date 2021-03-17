7120888a809cec595812e346fccae065d0761230
kampl50 <kamil.sitek98@interia.pl>
2021-03-17 23:36:07 +0100

    dodanie podstawowego widoku

--- frontend/psk-converter/src/app/app.module.ts
+++ frontend/psk-converter/src/app/app.module.ts
@@ -2,18 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ConverterMenuComponent } from './converter-menu/converter-menu.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+import { AppComponent } from './app.component';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent, ConverterMenuComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,
+    NgxMatFileInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/psk-converter/src/app/converter-menu/converter-menu.component.ts
+++ frontend/psk-converter/src/app/converter-menu/converter-menu.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { FormControl, Validators } from '@angular/forms';
+import {
+  AcceptValidator,
+  MaxSizeValidator,
+} from '@angular-material-components/file-input';
 
 @Component({
   selector: 'app-converter-menu',
   templateUrl: './converter-menu.component.html',
-  styleUrls: ['./converter-menu.component.css']
+  styleUrls: ['./converter-menu.component.css'],
 })
 export class ConverterMenuComponent implements OnInit {
+  foods: Food[] = [
+    { value: 'CSV', viewValue: 'CSV' },
+    { value: 'JSON', viewValue: 'JSON' },
+    { value: 'XML', viewValue: 'XML' },
+  ];
 
-  constructor() { }
+  color: ThemePalette = 'primary';
+  disabled: boolean = false;
+  multiple: boolean = false;
+  accept: string;
 
-  ngOnInit(): void {
+  fileControl: FormControl;
+
+  public options = [
+    { value: true, label: 'True' },
+    { value: false, label: 'False' },
+  ];
+
+  public listColors = ['primary', 'accent', 'warn'];
+  public listAccepts = [
+    null,
+    '.png',
+    'image/*',
+    '.doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  ];
+
+  public files;
+  maxSize = 16;
+
+  constructor() {
+    this.fileControl = new FormControl(this.files, [
+      Validators.required,
+      MaxSizeValidator(this.maxSize * 1024),
+    ]);
   }
 
+  ngOnInit() {
+    this.fileControl.valueChanges.subscribe((files: any) => {
+      if (!Array.isArray(files)) {
+        this.files = [files];
+      } else {
+        this.files = files;
+      }
+    });
+  }
+
+  onDisabledChanged(value: boolean) {
+    if (!value) {
+      this.fileControl.enable();
+    } else {
+      this.fileControl.disable();
+    }
+  }
 }
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+const presetFiles = [new File([], 'file 1'), new File([], 'file 2')];
+const presetFile = new File([], 'file 1');
